95da70a267ea9a9a6cf21f50f991a0d02f0be9fb
leonardo <leonardozw46@gmail.com>
2023-09-19 21:04:44 -0300

    feat: manga form component created

---
+++ frontend/manga-crud/src/app/manga/manga-form.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'mc-manga-form',
+  templateUrl: './manga-form.component.html',
+  styleUrls: ['./manga-form.component.css']
+})
+export class MangaFormComponent {
+
+}
