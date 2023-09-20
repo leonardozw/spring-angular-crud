293dd05506dd6e105e0aad22935fbbddcc79b53b
leonardo <leonardozw46@gmail.com>
2023-09-19 21:03:22 -0300

    feat: navbar component created

--- frontend/manga-crud/src/app/app.module.ts
+++ frontend/manga-crud/src/app/app.module.ts
@@ -2,21 +2,37 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { HttpClientModule } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { RouterModule } from '@angular/router';
 import { AppComponent } from './app.component';
+import { MangaDetailComponent } from './manga/manga-detail.component';
+import { MangaFormComponent } from './manga/manga-form.component';
 import { MangaComponent } from './manga/manga.component';
+import { NavbarComponent } from './navbar/navbar.component';
 import { MaterialModule } from './shared/material.module';
 
 @NgModule({
   declarations: [
     AppComponent,
-    MangaComponent
+    MangaComponent,
+    MangaDetailComponent,
+    NavbarComponent,
+    MangaFormComponent
   ],
   imports: [
     BrowserModule,
     HttpClientModule,
     BrowserAnimationsModule,
-    MaterialModule
+    MaterialModule,
+    FormsModule,
+    RouterModule.forRoot([
+      {path: 'mangas', component: MangaComponent},
+      {path: 'mangas/new', component: MangaFormComponent},
+      {path: 'mangas/id/:id', component: MangaDetailComponent},
+      {path: '', redirectTo: 'mangas', pathMatch: 'full'}, // default route
+      {path: '**', redirectTo: 'mangas', pathMatch: 'full'} // 404 route, is this case redirect to default route
+    ])
   ],
   providers: [],
   bootstrap: [AppComponent]
